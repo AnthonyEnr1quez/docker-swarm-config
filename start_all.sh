b9e95b1001fc7c5721cdcf431de7b971eabfa29e
AnthonyEnr1quez <aenriquez_dev@icloud.com>
2021-05-31 02:28:01 -0500

    caddy platform, fix typo

--- start_all.sh
+++ start_all.sh
@@ -7,7 +7,7 @@ docker-compose -f nfs_server.yml up -d
 docker-compose -f qbittorrent up -d
 
 base="NFS_CONN=${NFS_CONN} docker stack deploy"
-caddy="${base} -c caddy.yml -c caddy"
+caddy="${base} -c caddy.yml caddy"
 pihole="${base} -c pihole.yml pihole"
 portainer="${base} -c portainer.yml portainer"
 media="${base} -c jackett.yml -c plex.yml -c radarr.yml -c sonarr.yml media"
